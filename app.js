4cdd7c687125b8f6a8c43c56de8f00ab5eb627a7
fran-q <fdquiroga2017@gmail.com>
2023-02-19 02:19:08 -0300

    creada las rutas dentro del app e instalacion de nodemon

--- app.js
+++ app.js
@@ -4,5 +4,24 @@ const app = express();
 const port = 3000;
 const funca = console.log("el localhost funciona en el puerto " + port);
 
-app.listen(port, () => funca); 
+app.use(express.static("./public"));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/index.html"));
+});
+app.get('/detalles', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/productDetails.html"));
+});
+app.listen(port, () => funca);
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/productCart.html"));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"));
+});
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"));
+});
 
+//npm i nodemon
+//npx nodemon app
+// Con el primer comando instalan el nodemon y con el segundo lo ejecutan, para que los cambios se carguen de forma inmediata a la pagina web apenas guarden los cambios, lo anoto por las dudas
