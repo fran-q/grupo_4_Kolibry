920eda6f093ab09405d8282ce2858c3bb7d3c9db
nahhhhir <nahirihan513@gmail.com>
2023-02-19 18:56:26 -0300

    url tipografias ya esta en home, detalles y registro

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.get('/registro', (req, res) => {
     res.sendFile(path.resolve(__dirname,"./src/views/register.html"));
 });
 
+// tipogrgrafias que selecionamos para usar
+
+//<link rel="preconnect" href="https://fonts.googleapis.com">
+//<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+//<link href="https://fonts.googleapis.com/css2?family=Calligraffitti&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+
 //npm i nodemon
 //npx nodemon app.js
 // Con el primer comando instalan el nodemon y con el segundo lo ejecutan, para que los cambios se carguen de forma inmediata a la pagina web apenas guarden los cambios, lo anoto por las dudas
